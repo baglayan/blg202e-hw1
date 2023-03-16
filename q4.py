9f7d0d539c26d45a12ab3f6ba9a4f7d9d284c3a0
baglayan <baglayan19@itu.edu.tr>
2023-03-16 16:16:09 +0300

    Fix functionality

--- q4.py
+++ q4.py
@@ -1,32 +1,41 @@
 # Student Name : Meriç Bağlayan
-# Student ID   : 150190056
+#  Student ID   : 150190056
 # Course       : BLG 202E Numerical Methods 2023 Spring
 # Project      : Homework 1
+
+import sys
+
 
 def sign(x):
     return x / abs(x)
 
+
 def f(x: float, cvar: float):
-    result = float(x) ** (5) - cvar
+    try:
+        result = float(x) ** (5) - cvar
+    except OverflowError as err:
+        print("Overflowed: " + str(err))
+        quit()
     return result
 
-def interval_halving(a: float, b: float, epsilon: float, cvar: float):
+
+def interval_halving(a: float, b: float, steps: int, very_small_number: float, allowed_margin: float, cvar: float):
     u = f(a, cvar)
     v = f(b, cvar)
-    diff = b - a
+    error = b - a
 
-    midpoint = 0
+    midpoint = a + error
 
     i = 1
 
-    while True:
+    for k in range(1, steps):
         if (sign(u) == sign(v)):
             break
-        diff /= 2
-        midpoint = a + diff
+        error /= 2
+        midpoint = a + error
         w = f(midpoint, cvar)
 
-        if (abs(w) <= epsilon):
+        if (abs(w) < very_small_number or abs(error) <= allowed_margin):
             return midpoint
 
         if (sign(w) != sign(u)):
@@ -35,10 +44,15 @@ def interval_halving(a: float, b: float, epsilon: float, cvar: float):
         else:
             a = midpoint
             u = w
-        
+
     return midpoint
 
-a = float(input("a: "))
-epsilon = float(input("epsilon: "))
 
-print("x =  " + str(interval_halving(-a, a, epsilon, a)) + " such that |a^⅕ - x| ≤ epsilon")
+try:
+    a = float(input("a: "))
+    epsilon_input = float(input("epsilon: "))
+except ValueError as err:
+    print("Invalid input: " + str(err))
+    quit()
+
+print("x =  " + str(interval_halving(-a, a, sys.maxsize, sys.float_info.epsilon, epsilon_input, a)) + " such that |a^⅕ - x| ≤ epsilon")
